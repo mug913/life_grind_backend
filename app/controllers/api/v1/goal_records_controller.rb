7b41102d6ee05f009e6f40a3fdb3b9d74b1cfbe9
Mug <brad.alexander913@gmail.com>
2020-11-16 16:13:58 -0800

    fixed routing param typo in goal_record controller

--- app/controllers/api/v1/goal_records_controller.rb
+++ app/controllers/api/v1/goal_records_controller.rb
@@ -37,7 +37,7 @@ class Api::V1::GoalRecordsController < ApplicationController
   private
     
     def record_params
-        params.require(:record).permit( :date, :field_1_name, :field_1_type, :field_1_data,
+        params.require(:goal_record).permit( :date, :field_1_name, :field_1_type, :field_1_data,
                                         :field_2_name, :field_2_type, :field_2_data,
                                         :field_3_name, :field_3_type, :field_3_data)
     end
